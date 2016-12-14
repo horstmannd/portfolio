fd4083ef39557ba1bd577eb3496e0cf8ddfd48ff
Dustin Horstmann <dustinhorstmann@Sunnys-MacBook-Pro.local>
2016-12-14 12:49:37 -0600

    Update resumeBuilder with name and role. Append this index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,26 @@
-$("#main").append("Dustin Horstmann")
+// # BASICS
+
+// Append Method:
+// $("#main").append("Dustin Horstmann")
+
+// ## Data Types
+// var firstName = "James";
+// var age = 32;
+// console.log(firstname);
+
+// var awesomeThoughts = "I am Dustin and I am AWESOME!!";
+// console.log(awesomeThoughts);
+
+// Replace Method:
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+// Append variable to index.html
+// $("#main").append(funThoughts);
+
+var name = "Dustin Horstmann";
+var role = "Front-end Web Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
